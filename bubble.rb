c31f3c765bef3e7f3fcf372cb68e9d1476b958a8
parhaml <lee.p.parham@gmail.com>
2016-02-17 12:35:50 +0800

    Both methods done

--- bubble.rb
+++ bubble.rb
@@ -4,7 +4,7 @@ def bubble_sort(values, passes=1)
   while changes
     changes = false
     passes += 1
-    (0..(numbers.size-passes)).each do |first|
+    (0.upto(numbers.size-passes)).each do |first|
       if numbers[first] > numbers[first+1]
         numbers[first], numbers[first+1] = numbers[first+1], numbers[first]
         changes = true
@@ -21,3 +21,23 @@ def bubble_sort(values, passes=1)
 end
 
 bubble_sort([43577, 10, 2, 4, 3, 2, 3, 1, 4, 4, 5, 7, 8, 14125, 36345, 124, 12, 34753, 7345, 124235, 3547])
+
+
+def bubble_sort_by(list)
+  passes = list.size-2
+  while passes > 0
+    0.upto(passes) do |first|
+      if yield(list[first], list[first+1]) < 0
+        list[first], list[first+1] = list[first+1], list[first]
+      end
+    end
+    passes -= 1
+  end
+  list.inspect
+end
+
+
+
+
+puts bubble_sort_by(["hi", "hello", "hey"]) {|left, right| left.length - right.length}
+puts bubble_sort_by(["hi", "hello", "hey"]) {|right, left| left.length - right.length}
